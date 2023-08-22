71bc8474e8af743d82c6f8387b17ee83df23408e
Vinicius Vale <vinicius.oliveira@soufairycode.com>
2023-08-22 19:10:41 -0300

    teste add button

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "antd";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
+          <Button>Botão</Button>
         </p>
         <a
           className="App-link"
